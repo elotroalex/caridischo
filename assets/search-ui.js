6a475a35d537bd1501761493fef60b5f722467be
Alex Gil <colibri.alex@gmail.com>
2021-12-29 09:10:30 -0500

    search results thumb fixed

--- assets/search-ui.js
+++ assets/search-ui.js
@@ -1,3 +1,4 @@
+
 // Methods and jQuery UI for Wax search box
 function excerptedString(str) {
   str = str || ''; // handle null > string
@@ -18,17 +19,17 @@ function getThumbnail(item, url) {
   }
 }
 
-function displayResult(item, fields, url) {
+function displayResult(item, fields, url, lang) {
   var pid   = item.pid;
   var label = item.label || 'Untitled';
   var link  = item.permalink;
   var thumb = getThumbnail(item, url);
   var meta  = excerptedString(item.blurb);
 
-  return `<div class="result"><a href="${url}${link}">${thumb}<p><span class="title">${item.label}</span><br><span class="meta">${meta}</span></p></a></div>`;
+  return `<div class="result"><a href="${url}${lang}${link}">${thumb}<p><span class="title">${label}</span><br><span class="meta">${meta}</span></p></a></div>`;
 }
 
-function startSearchUI(fields, indexFile, url) {
+function startSearchUI(fields, indexFile, url, lang, the_word_results) {
   $.getJSON(indexFile, function(store) {
     var index  = new elasticlunr.Index;
 
@@ -44,12 +45,12 @@ function startSearchUI(fields, indexFile, url) {
       var results     = index.search(query, { boolean: 'AND', expand: true });
 
       results_div.empty();
-      results_div.append(`<p class="results-info">Displaying ${results.length} results</p>`);
+      results_div.append(`<p class="results-info">${results.length} ${the_word_results}</p>`);
 
       for (var r in results) {
         var ref    = results[r].ref;
         var item   = store[ref];
-        var result = displayResult(item, fields, url);
+        var result = displayResult(item, fields, url, lang);
 
         results_div.append(result);
       }
